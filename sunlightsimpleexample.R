8883988829d61dc60afa66edf0bb9efd07725842
Wagner Quiros <wquiros@umail.ucsb.edu>
2017-04-24 18:22:23 -0700

    Changed to year analysis

--- sunlightsimpleexample.R
+++ sunlightsimpleexample.R
@@ -1,6 +1,6 @@
 sunlight = read.table("sun.txt", header=T)
 par(mar=c(5,6,3,2))
-boxplot(sunlight$Kdown_direct~sunlight$month,
+boxplot(sunlight$Kdown_direct~sunlight$year,
         ylab="Downwelling Solar\n kj/m2/day",
         xlab="month", col="blue")
 
